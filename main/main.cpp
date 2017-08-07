bfb6db75117afbcec418ebc164a3d1b2df5c704c
ratnesh <ittefaq11@hotmail.com>
2017-08-07 06:47:50 -0700

    removed opencv dependency in cmake

--- main/main.cpp
+++ main/main.cpp
@@ -1,15 +1,8 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/core/utility.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
-using namespace cv;
-
 
 #include <dijkstra/dijkstra.h>
 #include "image-graph.h"
